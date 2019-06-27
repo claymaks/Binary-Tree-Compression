d2a5b43a373bb25c8ecd7fc9c96f117d49a9f62f
Schemberizer <51997759+Schemberizer@users.noreply.github.com>
2019-06-27 09:38:20 -0400

    Created a replacement class for the text2binary script

---
+++ Bit_counter.py
@@ -0,0 +1,7 @@
+#Bit counter
+
+def bit_counter():
+
+    char_in = input('Entrer name(with extension) of original character file: ')
+    bit_in = input('Enter name(with extentsion) of converted bit file: ')
+    enc_in = input('Enter name(with extension) of encoded bit file: ')
