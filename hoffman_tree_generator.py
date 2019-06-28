f32f180e9448610f2cf86aecd0f4661d458ac5f8
cmaks <cmaks@umich.edu>
2019-06-28 14:44:49 -0400

    we done

--- hoffman_tree_generator.py
+++ hoffman_tree_generator.py
@@ -1,70 +1,136 @@
 from tree import huffman
 from txtIO import fg, encode
 from listIO import listIO
+from Bit_counter import bitCounter_v2
+from sys import platform
+import time
+import os
 
-#Read in sample text, output a frequency as sorted_chars
+
+FILE = input("File: ")
+
+start = time.time()
+if ".txt" in FILE:
+    pass
+else:
+    print("Not a txt")
+    quit()
+
+if platform == "linux" or platform == "linux2":
+    fn = FILE.split("/")[-1].split(".")[0]
+elif platform == "darwin":
+    fn = FILE.split("/")[-1].split(".")[0]
+elif platform == "win32":
+    fn = FILE.split("\\")[-1].split(".")[0]
+PATH = FILE[:-(len(fn)+4)]
+LOG_F = PATH + "log.txt"
+LOG = open(LOG_F, "w")
+LOG.write(fn)
+LOG.close()
+
+LOG = open(LOG_F, "a")
+
+print("File size:", os.path.getsize(FILE), file=LOG)
+ENCODE_TIME_CONST = 9738.61561
+t = ((1.15**(1.25*(os.path.getsize(FILE)/ENCODE_TIME_CONST))) - 1) + os.path.getsize(FILE)/ENCODE_TIME_CONST
+print("Estimated time greater than:", t, "seconds.")
+print("Simulated on 2.3 GHz Intel Core i5")
+
+print("Estimated time greater than:", t, "seconds.", file=LOG)
+print("Simulated on 2.3 GHz Intel Core i5", file=LOG)
+
+
+#print("Read in sample text, output a frequency as sorted_chars")
 reader = fg()
-reader.read("sample_text.txt")
+reader.read(FILE)
 reader.gen_freq()
 sorted_chars = sorted(reader.characters, key=lambda x: x[1], reverse=False)
+#print(time.time()-start)
 
-#Create Huffman tree, initialize with sorted_chars
+#print("Create Huffman tree, initialize with sorted_chars")
 he = huffman()
 he.insert(sorted_chars)
-#encode sample text as Huffman encoded binary
+#print(time.time()-start)
+
+#print("encode sample text as Huffman encoded binary")
 e = encode()
-e.open_file("sample_text.txt")
+e.open_file(FILE)
 e.encode(he.find_char)
+#print(time.time()-start)
 
-#exports sorted_chars as list to text document
-theFile = 'sample_text.tree.txt'
+#print("strip sorted_chars of 3rd item (binary path)")
+stripped_chars = []
+for i in sorted_chars:
+    stripped_chars.append([i[0],i[1]])
+#print(time.time()-start)
 
-listIO.exportList(theFile,sorted_char)
+#print("exports sorted_chars as list to text document")
+theFile = PATH + fn +'.tree.txt'
 
-#import sample_text.tree.txt as list
+
+listIO.exportList(theFile,stripped_chars)
+#print(time.time()-start)
+
+#print("import sample_text.tree.txt as list")
 impList = listIO.importList(theFile)
+#print(time.time()-start)
 
-#load list into tree
+#print("load list into tree")
 hd = huffman()
 hd.insert(impList)
+#print(time.time()-start)
 
-#load sample_text.huffman.txt
+#print("load sample_text.huffman.txt")
 reader = fg()
-reader.read("sample_text.huffman.txt")
+readFile = PATH + fn + ".huffman.txt"
+reader.read(readFile)
+#print(time.time()-start)
 
-#decodes sample_text.huffman.txt
+#print("decodes sample_text.huffman.txt")
 string = ""
 for word in reader.reader:
     string += word
 new_string = ""
+bit_size = len(string)
+#print("Reading", bit_size, "bits")
+lock = False
 while string:
     char, string = hd.translate(string)
     new_string += char
+    
+#print(time.time()-start)
 
-#exports as sample_text.export.txt
-file = open("sample_text.export.txt", "w+")
+#print("exports as sample_text.export.txt")
+fileEx = PATH + fn + ".export.txt"
+file = open(fileEx, "w+")
 file.write(new_string)
 file.close()
+end = time.time()
 s = input("Files complete. Press \"s\" for stats >>> ")
 
 if s == "s":
     #ANALYZE FILES
-
-    pass
-    #print("Initial bits in file:", )
-
-    #print("Final bits in file:", )
+    print("Time to run:", end-start, file=LOG)
+    uncomp = bitCounter_v2(FILE, "s")
+    print("Initial bits in file:", uncomp, file=LOG)
+    comp = bitCounter_v2(readFile, "b")
+    print("Final bits in file:", comp, file=LOG)
 
     #uncompressed / compressed
-    #print("Compression ratio:", )
+    print("Compression ratio:", uncomp/comp, file=LOG)
 
-    #print("Huffman tree size:", )
+    hts = bitCounter_v2(theFile, "s")
+    print("Huffman tree size:", hts, "bits", file=LOG)
 
     #final bits + huffman tree bits
-    #print("Total size:", )
+    print("Total size:", comp + hts, file=LOG)
     
     #100 * (compressed + huffman tree) / uncompressed
-    #print("Space saved:", )
+    print("Transfer cutdown to:", int(100 * (comp + hts) / uncomp)+1, "%", file=LOG)
+    LOG.close()
+else:
+    LOG.close()
+    quit()
 
 
 
