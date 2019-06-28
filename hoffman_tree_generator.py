24ff9e37585deef6c2251fd6ba7ee6bbc07f9ec7
cmaks <cmaks@umich.edu>
2019-06-28 10:00:58 -0400

    fixed naming problem, needs dat anal and tree io

--- hoffman_tree_generator.py
+++ hoffman_tree_generator.py
@@ -1,6 +1,6 @@
 from tree import huffman
 from txtIO import fg, encode
-from listIO import 
+from listIO import listIO
 
 #Read in sample text, output a frequency as sorted_chars
 reader = fg()
@@ -9,17 +9,15 @@ reader.gen_freq()
 sorted_chars = sorted(reader.characters, key=lambda x: x[1], reverse=False)
 
 #Create Huffman tree, initialize with sorted_chars
-e = huffman()
-e.insert(sorted_chars)
-
+he = huffman()
+he.insert(sorted_chars)
 #encode sample text as Huffman encoded binary
 e = encode()
 e.open_file("sample_text.txt")
-e.encode(h.find_char)
+e.encode(he.find_char)
 
 #exports sorted_chars as list to text document
 '"sample_text.tree.txt"'
-
 
 
 #import sample_text.tree.txt as list
@@ -27,8 +25,8 @@ e.encode(h.find_char)
 
 
 #load list into tree
-d = huffman()
-d.insert(sorted_chars)
+hd = huffman()
+hd.insert(sorted_chars)
 
 #load sample_text.huffman.txt
 reader = fg()
@@ -39,10 +37,37 @@ string = ""
 for word in reader.reader:
     string += word
 new_string = ""
-wile string:
-    char, string = d.translate(string)
+while string:
+    char, string = hd.translate(string)
     new_string += char
 
 #exports as sample_text.export.txt
-file = open("sample_text.export.txt", "w")
+file = open("sample_text.export.txt", "w+")
 file.write(new_string)
+file.close()
+s = input("Files complete. Press \"s\" for stats >>> ")
+
+if s == "s":
+    #ANALYZE FILES
+
+
+    print("Initial bits in file:", )
+
+    print("Final bits in file:", )
+
+    #uncompressed / compressed
+    print("Compression ratio:", )
+
+    print("Huffman tree size:", )
+
+    #final bits + huffman tree bits
+    print("Total size:", )
+    
+    #100 * (compressed + huffman tree) / uncompressed
+    print("Space saved:", )
+
+
+
+
+
+    
